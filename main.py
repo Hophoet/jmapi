8c6344b54dc9fdfdc18f8de3b79af86454f32cbd
hophoet <hophoet@gmail.com>
2020-02-07 16:58:19 +0000

    add endpoints (users, messages getter, user login, user registery, user getting )

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI 
+from typing import List
+from fastapi import FastAPI, Depends, Body
+from sqlalchemy.orm import Session
+from app.database import get_database
+from app import crud
+from app import types
 
 #API main object
 app = FastAPI()
@@ -11,3 +16,44 @@ def root():
     return {
         'message':'Welcome on JM mobile application API'
     }
+
+#get messages endpoint
+@app.get('/messages/', response_model=List[types.Message])
+def read_messages(skip: int = 0, limit: int = 100, database: Session =  Depends(get_database)):
+    messages = crud.get_messages(database=database, skip=skip, limit=limit)
+    return messages
+
+#get users endpoint
+@app.get('/users/', response_model=List[types.User])
+def read_users(skip: int = 0, limit: int = 100, database: Session =  Depends(get_database)):
+    users = crud.get_users(database=database, skip=skip, limit=limit)
+    return users
+
+
+@app.post('/register/')
+def register(user: types.User = Body(..., embed=True),  database: Session =  Depends(get_database)):
+    exist_user = crud.get_user_by_phone_number(database, user.phone_number)
+    if exist_user: 
+        return { 
+            'text':'user already exists!',
+            'user':exist_user
+             }
+    db_user = crud.create_user(database, user)
+    exist_user = crud.get_user_by_phone_number(database, user.phone_number)
+    return {
+        'text': 'user created successfully!',
+        'user': exist_user
+        }
+
+@app.post('/login/')
+def login(phone_number: str = Body(..., embed=True),  database: Session =  Depends(get_database)):
+    exist_user = crud.get_user_by_phone_number(database, phone_number)
+    if exist_user: 
+        return { 
+            'text':'login successfully!',
+            'user':exist_user
+             }
+
+    return {
+        'text': 'invalid credental!'
+        }
